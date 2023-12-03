4cc06a07e28c1e400e150b4bf78c689cd09f0b51
SAMIL <ismailsamilacc@gmai.com>
2023-12-03 14:33:46 +0100

    Add comment what do you did into it

---
+++ prt2.js
@@ -0,0 +1,11 @@
+// const number = Math.trunc(Math.random() * 20) + 1;
+// console.log(number);
+document.querySelector(".check").addEventListener("click", function () {
+  console.log(document.querySelector(".guess").value);
+  // const guess = Number(document.querySelector(".guess").value);
+  // console.log(guess, typeof guess);
+  // console.log("number");
+  // if (!guess) {
+  //   document.querySelector(".pap").textContent = "uncorrect number";
+  // }
+});
